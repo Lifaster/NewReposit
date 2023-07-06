68327316f6a727c0b23e2ebdd068877e14c25ee6
Lifaster <vyuzhanin.ars2008@gmail.com>
2023-07-07 02:23:12 +1000

    6.3.1

--- нлрьврн/Program.cs
+++ нлрьврн/Program.cs
@@ -2,111 +2,39 @@
 
 namespace FirstApp
 {
-    class Human
+    class Company
     {
-        // Поля класса
-        public string name;
-        public int age;
-
-        // Метод класса
-        public void Greetings()
-        {
-            Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
-        }
-
-        // Конструктор 1
-        public Human()
-        {
-            name = "Неизвестно";
-            age = 20;
-        }
-        // Конструктор 2
-        public Human(string n)
-        {
-            name = n;
-            age = 20;
-        }
-        // Конструктор 3
-        public Human(string n, int a)
-        {
-            name = n;
-            age = a;
-        }
+        public string Type;
+        public string Name;
     }
 
-    class Pen
+    class Department
     {
-        public string color;
-        public int cost;
-
-        public Pen()
-        {
-            color = "Черный";
-            cost = 100;
-        }
-        public Pen(string a, int b)
-        {
-            color = a;
-            cost = b;
-        }
+        public Company Company;
+        public City City;
     }
 
-    class Rectangle
+    class City
     {
-        public int a;
-        public int b;
-
-        public Rectangle()
-        {
-            a = 6;
-            b = 4;
-        }
-
-        public Rectangle(int a, int b)
-        {
-            this.a = a;
-            this.b = b;
-        }
-
-        public Rectangle(int c)
-        {
-            a = c;
-            b = c;
-        }
-
-        public int Square()
-        {
-            return a * b;
-        }
-    }
-        struct Animal
-    {
-        // Поля структуры
-        public string type;
-        public string name;
-        public int age;
-
-        // Метод структуры
-        public void Info()
-        {
-            Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
-        }
+        public string Name;
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Human human = new Human();
-            human.Greetings();
+            var department = GetCurrentDepartment();
+        }
 
-            human = new Human("Дмитрий");
-            human.Greetings();
-
-            human = new Human("Дмитрий", 23);
-            human.Greetings();
-
-            Console.ReadKey();
+        static Department GetCurrentDepartment()
+        {
+            Department department = new Department();
+            string name;
+            if (department?.City?.Name == "Санкт-Петербург" && department?.Company?.Type == "Банк")
+            {
+                name = department?.Company?.Name ?? "Неизвестная компания";
+                Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", name);
+            }
         }
     }
 }
