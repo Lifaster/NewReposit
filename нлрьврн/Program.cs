3d5265298777a3af63eddab1befde7ba041addeb
Lifaster <vyuzhanin.ars2008@gmail.com>
2023-07-07 02:26:18 +1000

    6.3.2

--- нлрьврн/Program.cs
+++ нлрьврн/Program.cs
@@ -19,6 +19,22 @@ namespace FirstApp
         public string Name;
     }
 
+    class Bus
+    {
+        public int? Load;
+
+        public void PrintStatus()
+        {
+            if (Load.HasValue)
+            {
+                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+            }
+            else
+            {
+                Console.WriteLine("Автобус пуст!");
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
