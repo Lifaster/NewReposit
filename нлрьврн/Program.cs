9f20afe7ac662c0e70a5bc98e0fdd12a59c435b6
Lifaster <vyuzhanin.ars2008@gmail.com>
2023-07-11 17:34:26 +1000

    6.6.1

--- нлрьврн/Program.cs
+++ нлрьврн/Program.cs
@@ -1,56 +1,38 @@
 ﻿using System;
 
-namespace FirstApp
+class Program
 {
-    class Company
+    static void Main(string[] args)
     {
-        public string Type;
-        public string Name;
-    }
+        double x1 = 3;
+        double y1 = 3;
+        double x2 = -3;
+        double y2 = 3;
 
-    class Department
-    {
-        public Company Company;
-        public City City;
-    }
+        double x3 = -9;
+        double y3 = 9;
+        double x4 = 9;
+        double y4 = 17;
 
-    class City
-    {
-        public string Name;
-    }
+        double a1 = y2 - y1;
+        double b1 = x1 - x2;
+        double c1 = a1 * x1 + b1 * y1;
 
-    class Bus
-    {
-        public int? Load;
+        double a2 = y4 - y3;
+        double b2 = x3 - x4;
+        double c2 = a2 * x3 + b2 * y3;
 
-        public void PrintStatus()
+        double deter = a1 * b2 - a2 * b1;
+
+        if (deter == 0)
         {
-            if (Load.HasValue)
-            {
-                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
-            }
-            else
-            {
-                Console.WriteLine("Автобус пуст!");
-            }
+            Console.WriteLine("Прямые не пересекаются");
         }
-    }
-    class Program
-    {
-        static void Main(string[] args)
+        else
         {
-            var department = GetCurrentDepartment();
-        }
-
-        static Department GetCurrentDepartment()
-        {
-            Department department = new Department();
-            string name;
-            if (department?.City?.Name == "Санкт-Петербург" && department?.Company?.Type == "Банк")
-            {
-                name = department?.Company?.Name ?? "Неизвестная компания";
-                Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", name);
-            }
+            double x = (b2 * c1 - b1 * c2) / deter;
+            double y = (a1 * c2 - a2 * c1) / deter;
+            Console.WriteLine("Прямые пересекаются в точке ({0}, {1})", x, y);
         }
     }
 }
