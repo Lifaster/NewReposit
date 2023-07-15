191004f45d73fd1a260d1a296498d0396e7a3058
Lifaster <vyuzhanin.ars2008@gmail.com>
2023-07-16 07:11:26 +1000

    7.1.5

--- нлрьврн/Program.cs
+++ нлрьврн/Program.cs
@@ -2,174 +2,51 @@
 
 namespace FirstApp
 {
-    class Company
-    {
-        public string Type;
-        public string Name;
-    }
-
-    class Department
-    {
-        public Company Company;
-        public City City;
-    }
-
-    class City
+    class Employee
     {
         public string Name;
+        public int Age;
+        public int Salary;
     }
-    enum TurnDirection
+
+    class ProjectManager: Employee
+    { 
+        public string ProjectName;
+    }
+
+    class Developer : Employee
+    { 
+        public string ProgrammingLanguage; 
+    }
+
+    class Food
     {
-        None = 0,
-        Left,
-        Right
     }
-
-    class Car
+    class Fruit: Food
     {
-        private double Fuel;
-
-        private int Mileage;
-
-        private string color;
-
-        private TurnDirection turn;
-
-        public Car()
-        {
-            Fuel = 50;
-            Mileage = 0;
-            color = "White";
-        }
-
-        private void Move()
-        {
-            // Move a kilometer
-            Mileage++;
-            Fuel -= 0.5;
-        }
-
-        private void Turn(TurnDirection direction)
-        {
-            turn = direction;
-        }
-
-        public void FillTheCar()
-        {
-            Fuel = 50;
-        }
-
-        public string GetColor()
-        {
-            return color;
-        }
-
-        public void ChangeColor(string newColor)
-        {
-            if (color != newColor)
-                color = newColor;
-        }
-
-        public bool IsTurningLeft()
-        {
-            return turn == TurnDirection.Left;
-        }
-
-        public bool IsTurningRight()
-        {
-            return turn == TurnDirection.Right;
-        }
     }
-
-    class Bus
+    class Vegetable: Food
     {
-        public int? Load;
-
-        public void PrintStatus()
-        {
-            if (Load.HasValue)
-            {
-                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
-            }
-            else
-            {
-                Console.WriteLine("Автобус пуст!");
-            }
-        }
     }
-
-    class User
+    class Apple: Fruit
     {
-        private int age;
-        private string login;
-        private string email;
-        public int Age
-        {
-            get
-            {
-                return age;
-            }
-
-            set
-            {
-                if (value < 18)
-                {
-                    Console.WriteLine("Возраст должен быть не меньше 18");
-                }
-                else
-                {
-                    age = value;
-                }
-            }
-        }
-        public string Login
-        {
-            get
-            {
-                return login;
-            }
-            set
-            {
-                if (value.Length < 3)
-                {
-                    Console.WriteLine("Логин должен быть не менее 3 символов");
-                }
-                else
-                {
-                    login = value;
-                }
-            }
-        }
-        public string Email
-        {
-            get
-            {
-                return email;
-            }
-            set
-            {
-                if (value.Contains("@") == true)
-                {
-                    email = value;
-                }
-                else
-                {
-                    Console.WriteLine("Неверный email.");
-                }
-            }
-        }
     }
-        class Program
+    class Banana : Fruit
+    {
+    }
+    class Pear : Fruit
+    {
+    }
+    class Potato: Vegetable
+    {
+    }
+    class Carrot: Vegetable
+    {
+    }
+    class Program
         {
             static void Main(string[] args)
             {
-                User user = new User();
-
-                user.Age = 23;
-                user.Email = "uogdsjh@o";
-                Console.WriteLine(user.Age);
-
-                Console.ReadKey();
             }
         }
     }
