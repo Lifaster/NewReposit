fd5ef7a60d2ce24e04019a0f8e79af9b8511d538
Lifaster <vyuzhanin.ars2008@gmail.com>
2023-08-10 18:27:59 +1000

    7.5.3

--- нлрьврн/Program.cs
+++ нлрьврн/Program.cs
@@ -3,17 +3,28 @@ using System.Net.NetworkInformation;
 
 namespace FirstApp
 {
-    class obj
+    class Helper
     {
-        public string Name;
-        public object Description;
-        public static int MaxValue = 2000;
+        static void Swap(ref int num1, ref int num2)
+        {
+            int num3;
+            num3 = num1;
+            num1 = num2;
+            num2 = num3; 
+        }
     }
 
     class Program
+    {
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-            }
+            int num1 = 3;
+            int num2 = 58;
+
+            Helper.Swap(ref num1, ref num2);
+
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
         }
+    }
 }
