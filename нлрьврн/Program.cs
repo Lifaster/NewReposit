80f816490e8370fb2441be6b1e1e6661cfc268f0
Lifaster <vyuzhanin.ars2008@gmail.com>
2023-08-17 05:06:43 +1000

    7.6.2

--- нлрьврн/Program.cs
+++ нлрьврн/Program.cs
@@ -3,48 +3,21 @@ using System.Net.NetworkInformation;
 
 namespace FirstApp
 {
-    static class gu
+    class Car<T>
     {
-            public static int GetNegative(this int num)
-            {   
-                if (num > 0)
-                {
-                    return -num;
-                }
-                else
-                {
-                    return num;
-                }
-
-        }
-        public static int GetPositive(this int num)
-        {
-            if (num < 0)
-            {
-                return -num;
-            }
-            else
-            {
-                return num;
-            }
-        }
+        public T Engine;
     }
-  
+    class GasEngine
+    {
+    }
+    class ElectricEngine
+    {
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
-
-            Console.WriteLine(num1.GetNegative()); //-7
-            Console.WriteLine(num1.GetPositive()); //7
-            Console.WriteLine(num2.GetNegative()); //-13
-            Console.WriteLine(num2.GetPositive()); //13
-            Console.WriteLine(num3.GetNegative()); //0
-            Console.WriteLine(num3.GetPositive()); //0
         }
-   }
+    }
 }
